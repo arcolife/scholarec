e584f5609904c72e07897f10c89ca40dfb09a724
arcolife <arcshams@gmail.com>
2014-02-10 19:00:55 +0530

    http -> https in url of setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='scholarec',
       version='0.1',
       description='Recommendation engine for scholarly works',
-      url='http://github.com/arcolife/scholarec.git',
+      url='https://github.com/arcolife/scholarec.git',
       author='Archit Sharma',
       author_email='archit.py@gmail.com',
       license='GPL v3',
