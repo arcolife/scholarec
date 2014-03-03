2fe0bb8b8e204541831e0ce652bb6fca95313f2b
arcolife <arcshams@gmail.com>
2014-03-04 03:59:02 +0530

    new refined tests; LICENSE: standardized specification

--- scholarec/Base/DocumentClass.py
+++ scholarec/Base/DocumentClass.py
@@ -1,12 +1,35 @@
-'''
-This script parses the XML structure 
-passed to the object of Document Class in it.'''
+# -*- coding: utf-8 -*- 
 
-# import dependencies
-from xml.dom.minidom import parseString #,parse 
+## This file is part of ScholaRec.
+## A recommendation engine for Scholarly works.
+## Copyright (C) 2014  Archit Sharma <archit.py@gmail.com>
+##
+## ScholaRec is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## ScholaRec is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+""" This script parses the XML structure 
+passed to the object of Document Class."""
+
+## import general modules
+from urlparse \
+    import urlparse
+from xml.dom.minidom import \
+    parseString #,parse
+
+## import dependencies
 import xmltodict
-from BeautifulSoup import BeautifulStoneSoup as Soup
-from urlparse import urlparse
+from BeautifulSoup import \
+    BeautifulStoneSoup as Soup
 #import requests
 
 class DocumentArXiv(object):
