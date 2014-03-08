9ee53abbc828b7bb765111105da881a68e23bd1c
arcolife <arcshams@gmail.com>
2014-03-08 09:10:47 +0530

    changing tests order in run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python ./scholarec/Base/DocumentClass.py
 grep '<entry>' ./log/output/query_results.xml | wc -l
 python ./scholarec/__init__.py
+python ./scholarec/Base/DocumentClass.py
