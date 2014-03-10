a8b2724448a0fae78a10022db63343b832b4deb4
arcolife <arcshams@gmail.com>
2014-03-10 06:16:55 +0530

    remove echo from run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 /usr/bin/python ./scholarec/Base/DocumentClass.py
-echo "Tested ./scholarec/Base/DocumentClass.py"
 /usr/bin/python ./scholarec/Base/DocumentData.py
-echo "Tested ./scholarec/Base/DocumentData.py"
 /usr/bin/python ./scholarec/__init__.py
-echo "Tested ./scholarec/__init__.py"
 /usr/bin/python ./tests/test.py
-echo "Tested ./tests/test.py"
