9958007c8795dc2e308d16e24f20963c85fd527f
arcolife <arcshams@gmail.com>
2014-03-11 02:09:46 +0530

    change travis.yml: sudo /usr/bin/python -> python

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/python ./scholarec/Base/DocumentClass.py
-/usr/bin/python ./scholarec/Base/DocumentData.py
-/usr/bin/python ./scholarec/__init__.py
-/usr/bin/python ./tests/test.py
+python ./scholarec/Base/DocumentClass.py
+python ./scholarec/Base/DocumentData.py
+python ./scholarec/__init__.py
+python ./tests/test.py
