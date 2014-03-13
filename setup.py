a9e8185793d07ffae30b5a6ba213a62f0815b737
arcolife <arcshams@gmail.com>
2014-03-14 00:26:57 +0530

    add requrements in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages, tests
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='scholarec',
       version='0.5',
@@ -9,5 +13,6 @@ setup(name='scholarec',
       author_email='archit.py@gmail.com',
       #test_suite="tests",
       license='GPL v3',
+      install_requires=reqs,
       #packages=['scholarec'],
       zip_safe=False)
