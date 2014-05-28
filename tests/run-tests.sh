4931df5809780c48e7896df9d4411ac3badbb184
Archit Sharma <arcshams@gmail.com>
2014-05-28 18:15:55 +0530

    add new test for QueryES module

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,5 +2,6 @@
 
 python ./scholarec/Base/DocumentClass.py
 python ./scholarec/Base/DocumentData.py
+python ./scholarec/QueryES/DocumentOperations.py
 python ./scholarec/__init__.py
 python ./tests/test.py
