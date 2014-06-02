dccf44340e56c06ae7c4601e7db9b5383a797176
Archit Sharma <arcshams@gmail.com>
2014-06-02 18:32:13 +0530

    fix requirements.txt location issue (pip)

--- setup.py
+++ setup.py
@@ -2,18 +2,21 @@ from setuptools import find_packages, tests, setup
 #from distutils.core import setup
 from pip.req import parse_requirements
 import os
+import codecs
 
-#install_reqs = parse_requirements('requirements.txt')
-#reqs = [str(ir.req) for ir in install_reqs]
+def local_file(file):
+  return codecs.open(
+    os.path.join(os.path.dirname(__file__), file), 'r', 'utf-8'
+  )
+
 install_reqs = [
-    line.strip()
-    for line in open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines()
-    if line.strip() != ''
+  line.strip()
+  for line in local_file('requirements.txt').readlines()
+  if line.strip() != ''
 ]
 
 # Use the VERSION file to get ScholaRec version
 version_file = os.path.join(os.path.dirname(__file__), 'scholarec', 'VERSION')
-# version_file = os.path.abspath(os.path.join(os.path.split(__file__)[0], 'scholarec', 'VERSION'))
 with open(version_file) as fh:
     scholarec_version = fh.read().strip()
 
