b8271c969730519d9f88d5c067e604db6bc1e208
Archit Sharma <arcshams@gmail.com>
2014-06-02 17:35:33 +0530

    fix travis build error; repace >= with == in requirements to avoid breaks

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-pip install -r requirements.txt
-pip install wikipedia
-pip install -e .
+#pip install -r requirements.txt
+#pip install wikipedia
+#pip install -e .
+
+sudo python setup.py install
